ef17f082666c523ce56de27786fb729e2fd4c103
Fredrik Olsson <fredrik.olsson2@outlook.com>
2018-03-16 18:03:26 +0100

    updated example

--- example/index.js
+++ example/index.js
@@ -1,21 +1,32 @@
 function onload(event){
    
    var starRating = rater({element:document.querySelector("#rater"), rateCallback: function rateCallback(rating) {
- 	starRating.setRating(rating);
+ 	starRating.setAvgRating(rating);
         starRating.disable();
 	}});
 
     var starRating2 = rater(
 	    {
+		max:5,
+		rating:4, 
+		element: document.querySelector("#rater2"),
+		disableText:"Custom disable text!",
+		ratingText:"My custom rating text {rating}", 
+		rateCallback: function rateCallback(rating) {
+			starRating2.setAvgRating(rating);
+			starRating2.disable();
+		}
+	});
+
+	var starRating3 = rater(
+	    {
 		max:16,
 		readOnly:true,
 		rating:4, 
-		element: document.querySelector("#rater2"),
-		disableText:"Thanks!",
-		ratingText:"My custom rating text {rating}", 
+		element: document.querySelector("#rater3"),
 		rateCallback: function rateCallback(rating) {
-			starRating2.setRating(rating);
-			starRating2.disable();
+			starRating3.setAvgRating(rating);
+			starRating3.disable();
 		}
 	});
 }
